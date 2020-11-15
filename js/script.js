86fd9f3de86e658ea67b2f7f1d30b399e5289475
Laurene-Ipsum <laureneipsum@gmail.com>
2020-11-15 18:18:12 +0100

    ADD- Scrool bar blue

--- js/script.js
+++ js/script.js
@@ -174,4 +174,23 @@ if(screen.width <= 1024) {
 // );
 
 
-
+$(document).ready(function(){
+    $(window).on('scroll', function () {
+    
+    var elmt   = $('.from-left, .from-right');
+    var topImg = $('.from-left, .from-right').offset().top;
+    var scroll = $(window).scrollTop();
+    
+        $(elmt).each(function() {
+            
+            var topImg = $(this).offset().top - 300;
+            
+                if ( topImg < scroll ) {
+                
+                $(this).css("transform", "translate(0,0)");
+                $(this).css("opacity", "1");
+                
+                };
+        });
+    });
+});
