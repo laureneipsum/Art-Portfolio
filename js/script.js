4e87c06c19ae9d8aa67f4738f407fc51ab3c9256
Laurene-Ipsum <laureneipsum@gmail.com>
2020-11-18 09:18:31 +0100

    Autoprefixer CSS / clean HTML & JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-//=========================================//
 //===========ANIMATION SOURIS============= //
-//=========================================//
 let mouseCursor = document.querySelector(".cursor");
 let menu_anim_cursor = document.querySelectorAll('#menu-burger');
 let title_animate_mouse = document.querySelectorAll('.title1');
@@ -27,39 +25,26 @@ let title_animate_mouse = document.querySelectorAll('.title1');
           mouseCursor.classList.add("text-pict");
       });
 	});
-
-//=========================================//
 //===========ANIMATION BURGER============= //
-//=========================================//
 
-//Récupération des blocs
 var mainMenu = document.querySelector("#menu");
 var burgerMenu = document.querySelector("#menu-burger");
+var clickedEvent = "click"; 
 
-/*===============================*/
-/*=== Clic sur le menu burger ===*/
-/*===============================*/
-// Vérifie si l'événement touchstart existe et est le premier déclenché
-var clickedEvent = "click"; // Au clic si "touchstart" n'est pas détecté
 window.addEventListener('touchstart', function detectTouch() {
-	clickedEvent = "touchstart"; // Transforme l'événement en "touchstart"
+	clickedEvent = "touchstart"; 
 	window.removeEventListener('touchstart', detectTouch, false);
 }, false);
 
-// Créé un "toggle class" en Javascrit natif (compatible partout)
 burgerMenu.addEventListener(clickedEvent, function(evt) {
 	console.log(clickedEvent);
-	// Modification du menu burger
+	
 	if(!this.getAttribute("class")) {
         this.setAttribute("class", "clicked");
         
 	} else {
 		this.removeAttribute("class"); }
     
-	// Variante avec x.classList (ou DOMTokenList), pas 100% compatible avant IE 11...
-	// burgerMenu.classList.toggle("clicked");
-
-	// Créé l'effet pour le menu slide (compatible partout)
 	if(mainMenu.getAttribute("class") != "visible") {
 		mainMenu.setAttribute("class", "visible");
 	} else {
@@ -67,7 +52,8 @@ burgerMenu.addEventListener(clickedEvent, function(evt) {
 	}
 }, false
 );
-//-----modifier clique bg-----//
+
+/*===============================*/
 document.body.onclick = function(e){
     var evt = window.event || e; 
     var target = evt.target || evt.srcElement; 
@@ -87,42 +73,38 @@ document.body.onclick = function(e){
     return true;
   }
 
-/*===============================*/
-/*=== Swipe avec Touch Events ===*/
-/*===============================*/
-// Si l'écran est plus petit que "x" pixels (optionnel) // 1024px ici
 if(screen.width <= 1024) {
-	var startX = 0; // Position de départ
-	var distance = 100; // 100 px de swipe pour afficher le menu
+	var startX = 0; 
+	var distance = 100; 
 
-	// Au premier point de contact
+
 	window.addEventListener("touchstart", function(evt) {
-		// Récupère les "touches" effectuées
+	
 		var touches = evt.changedTouches[0];
 		startX = touches.pageX;
 		between = 0;
 	}, false);
 
-	// Quand les points de contact sont en mouvement
+	
 	window.addEventListener("touchmove", function(evt) {
-		// Limite les effets de bord avec le tactile...
+		
 		evt.preventDefault();
 		evt.stopPropagation();
 	}, false);
 
-	// Quand le contact s'arrête
+	
 	window.addEventListener("touchend", function(evt) {
 		var touches = evt.changedTouches[0];
 		var between = touches.pageX - startX;
 
-		// Détection de la direction
+		
 		if(between > 0) {
 			var orientation = "ltr";
 		} else {
 			var orientation = "rtl";
 		}
 
-		// Modification du menu burger
+		
 		if(Math.abs(between) >= distance && orientation == "ltr" && mainMenu.getAttribute("class") != "visible") {
 				burgerMenu.setAttribute("class", "clicked");
 		}
@@ -139,20 +121,14 @@ if(screen.width <= 1024) {
 		}
 	}, false);
 }
-// ==========================================//
-// =============CLOSE MENU ================ //
-// =========================================//
-// ========================================//
 
+// =============CLOSE MENU ================ //
 document.getElementById("sendmail").addEventListener("submit",function() {
 	e.preventDefault();
 	alert('Message bien reçu ;-)');
 }
-
 )
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
-///////////////////////////////////////////////////
+
 //---------GALERIE 1-----------------//
 function openModal() {
 	document.getElementById("myModal").style.display = "block";
